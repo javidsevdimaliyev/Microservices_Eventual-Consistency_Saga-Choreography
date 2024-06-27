9570c368d5d484b09a2cf27916b02c09759603bd
javidsevdimaliyev <javid.sevdimaliyev@outlook.com>
2024-06-27 15:10:43 +0400

    first commit

---
+++ Payment.API/Consumers/StockReservedEventConsumer.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Shared.Events;
+
+namespace Payment.API.Consumers
+{
+    public class StockReservedEventConsumer(IPublishEndpoint publishEndpoint) : IConsumer<StockReservedEvent>
+    {
+        public async Task Consume(ConsumeContext<StockReservedEvent> context)
+        {
+            Random random = new Random();
+            bool paymentResult= random.Next(2) == 0;
+            if (paymentResult)
+            {
+                //Payment is successful...
+                PaymentCompletedEvent paymentCompletedEvent = new()
+                {
+                    OrderId = context.Message.OrderId
+                };
+                await publishEndpoint.Publish(paymentCompletedEvent);
+                await Console.Out.WriteLineAsync("Payment is successful...");
+            }
+            else
+            {
+                //Payment failed...
+                PaymentFailedEvent paymentFailedEvent = new()
+                {
+                    OrderId = context.Message.OrderId,
+                    Message = "Insufficient balance...",
+                    OrderItems = context.Message.OrderItems
+                };
+                await publishEndpoint.Publish(paymentFailedEvent);
+                await Console.Out.WriteLineAsync("Payment failed...");
+            }
+        }
+    }
+}
---
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,64 @@
+﻿using MassTransit;
+using MongoDB.Driver;
+using Shared;
+using Shared.Events;
+using Stock.API.Services;
+
+namespace Stock.API.Consumers
+{
+    public class OrderCreatedEventConsumer(MongoDBService mongoDBService, ISendEndpointProvider sendEndpointProvider, IPublishEndpoint publishEndpoint) : IConsumer<OrderCreatedEvent>
+    {
+        //readonly MongoDBService _mongoDBService;
+
+        //public OrderCreatedEventConsumer(MongoDBService mongoDBService)
+        //{
+        //    _mongoDBService = mongoDBService;
+        //}
+
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            List<bool> stockResult = new();
+            IMongoCollection<Models.Stock> collection = mongoDBService.GetCollection<Models.Stock>();
+
+            foreach (var orderItem in context.Message.OrderItems)
+            {
+                stockResult.Add(await (await collection.FindAsync(s => s.ProductId == orderItem.ProductId.ToString() && s.Count > (long)orderItem.Count)).AnyAsync());
+            }
+
+            if (stockResult.TrueForAll(s => s.Equals(true)))
+            {
+                //Stock update...
+                foreach (var orderItem in context.Message.OrderItems)
+                {
+                    Models.Stock stock = await (await collection.FindAsync(s => s.ProductId == orderItem.ProductId.ToString())).FirstOrDefaultAsync();
+                    stock.Count -= orderItem.Count;
+
+                    await collection.FindOneAndReplaceAsync(x => x.ProductId == orderItem.ProductId.ToString(), stock);
+                }
+                //Throwing the event that will warn Payment...
+                var sendEndpoint = await sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettings.Payment_StockReservedEventQueue}"));
+                StockReservedEvent stockReservedEvent = new()
+                {
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    TotalPrice = context.Message.TotalPrice,
+                    OrderItems = context.Message.OrderItems,
+                };
+                await sendEndpoint.Send(stockReservedEvent);
+            }
+            else
+            {
+                //Stock operation failed...
+                //Event will be thrown to warn Order.
+                StockNotReservedEvent stockNotReservedEvent = new()
+                {
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    Message = "Inadequate stock quantity..."
+                };
+
+                await publishEndpoint.Publish(stockNotReservedEvent);
+            }
+        }
+    }
+}
